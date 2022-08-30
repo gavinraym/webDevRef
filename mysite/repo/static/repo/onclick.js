c4b40783cc3800a4fe68ad9180e1333faf8c7190
Gavin Ray <gavinraym@gmail.com>
2022-08-30 00:41:46 -0700

    Adding onclick

---
+++ mysite/repo/static/repo/onclick.js
@@ -0,0 +1,20 @@
+
+function start() {
+    // let squares = document.getElementsByClassName("square")
+    // for (let i =0; i<squares.length; i++) {
+    //     let x
+    //     squares.item(i).addEventListener("click", click_square())
+    // }
+
+}
+
+
+function hello() {
+    let h1 = document.querySelector('h1');
+    h1.textContent = "Hello World";
+}
+
+function click_square(x) {
+    let h1 = document.querySelectorAll('h1')[1];
+    h1.textContent = x;
+}
